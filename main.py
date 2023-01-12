638318d52e0bc10338d49ec2711951ee1e8a76a5
Andrzej Tokajuk <01178611@pw.edu.pl>
2023-01-12 20:57:28 +0100

    No more lags when backspace

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import curses
+from curses import wrapper
 import datetime
-import time
+from time import sleep
+import random
 
 
 def pick_text():
@@ -11,72 +13,62 @@ def itereate():
     pass
 
 
-def print_typed_text(stdscr, target_text, typed_text_list):
+def countcown(window):
+    window.addstr(0, 0, "3..")
+    window.refresh()
+    sleep(1)
+    window.addstr(0, 3, "2..")
+    window.refresh()
+    sleep(1)
+    window.addstr(0, 6, "1..\n")
+    window.refresh()
+    sleep(1)
+
+
+def print_typed_text(window, target_text, typed_text_list):
     target_text_list = [*target_text]
     i = 0
     while i <= len(target_text):
-        stdscr.refresh()
-        # stdscr.addstr(target_text)
-        # stdscr.addstr('\n')
 
         for inx, char in enumerate(typed_text_list):
             if char == target_text_list[inx]:
-                stdscr.addstr(1, inx, char, curses.color_pair(1))
+                window.addstr(2, inx, char, curses.color_pair(1))
             else:
-                stdscr.addstr(1, inx, char, curses.color_pair(2))
+                window.addstr(2, inx, char, curses.color_pair(2))
 
-        typed_char = stdscr.getkey()
+        typed_char = window.getkey()
 
         if typed_char == 'KEY_BACKSPACE':
             if typed_text_list:
                 typed_text_list.pop()
                 i -= 1
-                stdscr.clear()
-                stdscr.addstr(target_text)
-                stdscr.addstr('\n')
+                window.erase()
+                window.addstr(1, 0, target_text)
+                window.addstr('\n')
         else:
             typed_text_list.append(typed_char)
             i += 1
 
 
-def main(stdscr):
+def main(window):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    stdscr.clear()
+    window.clear()
+
     target_text = "To nie jest koncowy tekst"
     target_text_list = [*target_text]
     typed_text_list = []
 
-    stdscr.addstr(target_text)
-    stdscr.addstr('\n')
+    window.addstr(1, 0, target_text)
+    window.addstr('\n')
+    countcown(window)
+    # window.refresh()
 
-    i = 0
-    while i <= len(target_text):
-        stdscr.refresh()
-        # stdscr.addstr(target_text)
-        # stdscr.addstr('\n')
 
-        for inx, char in enumerate(typed_text_list):
-            if char == target_text_list[inx]:
-                stdscr.addstr(1, inx, char, curses.color_pair(1))
-            else:
-                stdscr.addstr(1, inx, char, curses.color_pair(2))
+    print_typed_text(window, target_text, typed_text_list)
 
-        typed_char = stdscr.getkey()
-
-        if typed_char == 'KEY_BACKSPACE':
-            if typed_text_list:
-                typed_text_list.pop()
-                i -= 1
-                stdscr.clear()
-                stdscr.addstr(target_text)
-                stdscr.addstr('\n')
-        else:
-            typed_text_list.append(typed_char)
-            i += 1
-
-    stdscr.getkey()
+    window.getkey()
 
 
 if __name__ == "__main__":
-    curses.wrapper(main)
+    wrapper(main)
